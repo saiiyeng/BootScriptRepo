9fd06cadb438c30ddcd6f5251e147aed7a9bfdda
Sainath SI <sainathsi@in.ibm.com>
2016-12-20 14:28:26 +0530

    updated script

--- testboot.sh
+++ testboot.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+touch /tmp/parablutestbootscript
+touch parablutestbootscript001
+mkdir saitest
 date=`date`
-echo " Start boot script test...$date"
+echo " Start boot script test...$date" >> parablutestbootscript001
 hostname=`/bin/hostname`
-echo " AWS VM = $hostname"
-echo " End boot script test !"
-
+echo " AWS VM = $hostname" >> parablutestbootscript001
+echo " End boot script test !" >> parablutestbootscript001
