6318f0fa4768c6f6f6816f96eb0e2969e11d0ae9
AnishAnil <anish.a.anil@gmail.com>
2017-04-24 13:26:50 +0530

    R

--- bolt_bootscript.sh
+++ bolt_bootscript.sh
@@ -12,4 +12,4 @@ mkdir /tmp/A2
 #sudo wget http://files.trendmicro.com/products/deepsecurity/en/9.6/Agent-RedHat_EL7-9.6.2-7723.x86_64.zip 
 #sudo unzip Agent-RedHat_EL7-9.6.2-7723.x86_64.zip
 #sudo rpm -i Agent-Core-RedHat_EL7-9.6.2-7723.x86_64.rpm
-#sudo /opt/ibm/dsa_control -a dsm://atl-prd-tmdsm-01a.mss.iss.net:4120/ tenantID:D1D85977-4681-779E-D9F8-4AF4B91B3428 tenantPassword:29358A02-8271-267C-DD1B-0BB0DACA3797
+#sudo /opt/ds_agent/dsa_control -a dsm://atl-prd-tmdsm-01a.mss.iss.net:4120/ tenantID:D1D85977-4681-779E-D9F8-4AF4B91B3428 tenantPassword:29358A02-8271-267C-DD1B-0BB0DACA3797
