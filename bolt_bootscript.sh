ed4c355a718e53e4a4d42a6fee65771645259a2e
AnishAnil <anish.a.anil@gmail.com>
2017-04-24 12:36:21 +0530

    Create a bolt script file

---
+++ bolt_bootscript.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Starting Installation " >> /tmp/BOLT_Install_Boot.log
+sudo -i
+whoami
+ls
+sed -i -e 's/Defaults requiretty.*/ #Defaults requiretty/g' /etc/sudoers
+yum install wget -y >> /tmp/wget.log 2>&1
+yum install unzip -y >> /tmp/BAAS_Install_Boot.log 2>&1
+
+sudo mkdir /tmp/Installer
+sudo cd /tmp/Installer
+sudo wget http://files.trendmicro.com/products/deepsecurity/en/9.6/Agent-RedHat_EL7-9.6.2-7723.x86_64.zip 
+sudo unzip Agent-RedHat_EL7-9.6.2-7723.x86_64.zip
+sudo rpm -i Agent-Core-RedHat_EL7-9.6.2-7723.x86_64.rpm
+sudo /opt/ibm/dsa_control -a dsm://atl-prd-tmdsm-01a.mss.iss.net:4120/ tenantID:D1D85977-4681-779E-D9F8-4AF4B91B3428 tenantPassword:29358A02-8271-267C-DD1B-0BB0DACA3797
